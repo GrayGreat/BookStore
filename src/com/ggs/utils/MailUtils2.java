c32c95f940ccd163a6eeb4222bc83f1f216c404d
GrayGreat <1285226024@qq.com>
2020-03-26 16:14:20 +0800

    修复项目在云服务器中无法发送邮件的bug

--- src/com/ggs/utils/MailUtils2.java
+++ src/com/ggs/utils/MailUtils2.java
@@ -1,6 +1,5 @@
 package com.ggs.utils;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -15,23 +14,18 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
 /**
- * @author Not Today
- * @date 2019年8月11日-下午4:59:13
+ *  @author Not Today
+ *  @date 2019年8月11日-下午4:59:13
  */
-public class MailUtils {
+public class MailUtils2 {
 
 	public static void sendMail(String email, String emailMsg) throws AddressException, MessagingException {
 		// 1.创建一个程序与邮件服务器会话对象 Session
 
 		Properties props = new Properties();
 		props.setProperty("mail.transport.protocol", "SMTP");
-		props.setProperty("mail.smtp.host", "smtp.126.com");
+		props.setProperty("mail.host", "smtp.126.com");
 		props.setProperty("mail.smtp.auth", "true");// 指定验证为true
-
-		// 阿里云禁用25端口,这里将邮件发送端口修改为ssl的465端口
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.port", "465");
 
 		// 创建验证器
 		Authenticator auth = new Authenticator() {
@@ -40,30 +34,23 @@ public class MailUtils {
 			}
 		};
 
-		// 使用过环境属性和授权信息,创建邮件会话
+
 		Session session = Session.getInstance(props, auth);
 
 		// 2.创建一个Message，它相当于是邮件内容
 		Message message = new MimeMessage(session);
 
-		try {
-			message.setFrom(new InternetAddress("a1285226024@126.com", "Starbug")); // 设置发送者
+		message.setFrom(new InternetAddress("a1285226024@126.com")); // 设置发送者
 
-			message.setRecipient(RecipientType.TO, new InternetAddress(email)); // 设置发送方式与接收者
+		message.setRecipient(RecipientType.TO, new InternetAddress(email)); // 设置发送方式与接收者
 
-			message.setSubject("用户激活");
-			// message.setText("这是一封激活邮件，请<a href='#'>点击</a>");
+		message.setSubject("用户激活");
+		// message.setText("这是一封激活邮件，请<a href='#'>点击</a>");
 
-			message.setContent(emailMsg, "text/html;charset=utf-8");
+		message.setContent(emailMsg, "text/html;charset=utf-8");
 
-			// 3.创建 Transport用于将邮件发送
+		// 3.创建 Transport用于将邮件发送
 
-			Transport.send(message);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			String err = e.getMessage();
-			// 在这里处理message内容， 格式是固定的
-			System.out.println(err);
-		}
+		Transport.send(message);
 	}
 }
